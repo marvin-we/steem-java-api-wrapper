942ea776391108c7fcc720894c43d95ecf3bd23b
Marvin Weck <Marvin Weck>
2017-05-07 19:32:45 +0200

    Made Price object ByteTransformable to support limit_order_create2_operation

--- src/main/java/eu/bittrade/libs/steem/api/wrapper/models/Price.java
+++ src/main/java/eu/bittrade/libs/steem/api/wrapper/models/Price.java
@@ -1,6 +1,12 @@
 package eu.bittrade.libs.steem.api.wrapper.models;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import eu.bittrade.libs.steem.api.wrapper.exceptions.SteemInvalidTransactionException;
+import eu.bittrade.libs.steem.api.wrapper.interfaces.ByteTransformable;
 
 /**
  * This class is the java implementation of the <a href=
@@ -9,28 +15,56 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  * 
  * @author <a href="http://steemit.com/@dez1337">dez1337</a>
  */
-public class Price {
+public class Price implements ByteTransformable {
     private Asset base;
     private Asset quote;
 
     /**
-     * 
-     * @return
+     * @return the base
      */
     public Asset getBase() {
         return base;
     }
 
     /**
-     * 
-     * @return
+     * @param base1
+     *            the base to set
+     */
+    public void setBase(Asset base1) {
+        base = base1;
+    }
+
+    /**
+     * @return the quote
      */
     public Asset getQuote() {
         return quote;
+    }
+
+    /**
+     * @param quote
+     *            the quote to set
+     */
+    public void setQuote(Asset quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public byte[] toByteArray() throws SteemInvalidTransactionException {
+        try (ByteArrayOutputStream serializedPriceObject = new ByteArrayOutputStream()) {
+            serializedPriceObject.write(this.getBase().toByteArray());
+            serializedPriceObject.write(this.getQuote().toByteArray());
+
+            return serializedPriceObject.toByteArray();
+        } catch (IOException e) {
+            throw new SteemInvalidTransactionException(
+                    "A problem occured while transforming the object into a byte array.", e);
+        }
     }
 
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this);
     }
+
 }
