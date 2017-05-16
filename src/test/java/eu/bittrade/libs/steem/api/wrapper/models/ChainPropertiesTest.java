bf201ae81ff3c1d79c5590f6234ec98d92db35e2
Marvin Weck <Marvin Weck>
2017-05-16 21:36:14 +0200

    Implemented chain property test

--- src/test/java/eu/bittrade/libs/steem/api/wrapper/models/ChainPropertiesTest.java
+++ src/test/java/eu/bittrade/libs/steem/api/wrapper/models/ChainPropertiesTest.java
@@ -1,6 +1,12 @@
 package eu.bittrade.libs.steem.api.wrapper.models;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.bitcoinj.core.Utils;
 import org.junit.Test;
+
+import eu.bittrade.libs.steem.api.wrapper.enums.AssetSymbolType;
 
 /**
  * Test the ChainProperties object.
@@ -9,8 +15,22 @@ import org.junit.Test;
  *
  */
 public class ChainPropertiesTest {
+    private final String EXPECTED_BYTE_REPRESENTATION = "881300000000000003535445454d0000ffff000000000000";
+
     @Test
     public void testSteemChainPropertiesToByteArray() throws Exception {
-        // TODO: Implement
+        ChainProperties chainProperties = new ChainProperties();
+
+        Asset accountCreationFee = new Asset();
+        accountCreationFee.setAmount(5000);
+        accountCreationFee.setSymbol(AssetSymbolType.STEEM);
+
+        chainProperties.setAccountCreationFee(accountCreationFee);
+        chainProperties.setMaximumBlockSize(65535);
+        chainProperties.setSdbInterestRate(0);
+
+        assertThat("Expect that the asset object has the given byte representation.",
+                Utils.HEX.encode(chainProperties.toByteArray()), equalTo(EXPECTED_BYTE_REPRESENTATION));
+
     }
 }
