038e40ae4d0f0917965a10d066bef61b73f026b3
marvin-we <marvin.weck@web.de>
2017-07-29 12:41:21 +0200

    Close #29 - Remove Subscriptions code as this feature no longer supported

--- src/main/java/eu/bittrade/libs/steemj/SteemApiWrapper.java
+++ src/main/java/eu/bittrade/libs/steemj/SteemApiWrapper.java
@@ -1736,48 +1736,4 @@ public class SteemApiWrapper {
 
         return communicationHandler.performRequest(requestObject, PostsPerAuthorPair.class);
     }
-
-    public void setSubscriptionCallback() throws SteemCommunicationException {
-        RequestWrapperDTO requestObject = new RequestWrapperDTO();
-        requestObject.setApiMethod(RequestMethods.SET_SUBSCRIBE_CALLBACK);
-        requestObject.setSteemApi(SteemApis.DATABASE_API);
-
-        Object[] parameters = { 4, true };
-        requestObject.setAdditionalParameters(parameters);
-
-        System.out.println(communicationHandler.performRequest(requestObject, String.class).get(0));
-    }
-
-    public void setBlockAppliedCallback() throws SteemCommunicationException {
-        RequestWrapperDTO requestObject = new RequestWrapperDTO();
-        requestObject.setApiMethod(RequestMethods.SET_BLOCK_APPLIED_CALLBACK);
-        requestObject.setSteemApi(SteemApis.DATABASE_API);
-
-        Object[] parameters = { 4 };
-        requestObject.setAdditionalParameters(parameters);
-
-        System.out.println(communicationHandler.performRequest(requestObject, String.class).get(0));
-    }
-
-    public void setPendingTransactionCallback() throws SteemCommunicationException {
-        RequestWrapperDTO requestObject = new RequestWrapperDTO();
-        requestObject.setApiMethod(RequestMethods.SET_PENDING_TRANSACTION_CALLBACK);
-        requestObject.setSteemApi(SteemApis.DATABASE_API);
-
-        Object[] parameters = { 4 };
-        requestObject.setAdditionalParameters(parameters);
-
-        System.out.println(communicationHandler.performRequest(requestObject, String.class).get(0));
-    }
-
-    public void cancelAllSubscriptions() throws SteemCommunicationException {
-        RequestWrapperDTO requestObject = new RequestWrapperDTO();
-        requestObject.setApiMethod(RequestMethods.CANCEL_ALL_SUBSCRIPTIONS);
-        requestObject.setSteemApi(SteemApis.DATABASE_API);
-
-        Object[] parameters = { 4, true };
-        requestObject.setAdditionalParameters(parameters);
-
-        System.out.println(communicationHandler.performRequest(requestObject, String.class).get(0));
-    }
 }
