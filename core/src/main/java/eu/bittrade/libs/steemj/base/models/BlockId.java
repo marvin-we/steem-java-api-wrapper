3b98b917abda66f5dd66bac73c427b9359f2a944
marvin-we <marvin.weck@web.de>
2017-09-16 16:26:19 +0200

    Add missing JavaDoc for Ripemd160 implementations

--- core/src/main/java/eu/bittrade/libs/steemj/base/models/BlockId.java
+++ core/src/main/java/eu/bittrade/libs/steemj/base/models/BlockId.java
@@ -6,6 +6,13 @@ package eu.bittrade.libs.steemj.base.models;
  * @author <a href="http://steemit.com/@dez1337">dez1337</a>
  */
 public class BlockId extends Ripemd160 {
+    /** Generated serial version uid. */
+    private static final long serialVersionUID = 5819705609731889206L;
+
+    /**
+     * @param hashValue
+     *            The ripemd160 hash.
+     */
     public BlockId(String hashValue) {
         super(hashValue);
     }
