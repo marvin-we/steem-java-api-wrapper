7f78245c1a15e89eb773d3e106f008a92c1d13c4
marvin-we <marvin.weck@web.de>
2017-09-17 16:16:20 +0200

    Work on #95

--- core/src/test/java/eu/bittrade/libs/steemj/base/models/operations/ProveAuthorityOperationIT.java
+++ core/src/test/java/eu/bittrade/libs/steemj/base/models/operations/ProveAuthorityOperationIT.java
@@ -1,11 +1,17 @@
 package eu.bittrade.libs.steemj.base.models.operations;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.ArrayList;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import eu.bittrade.libs.steemj.BaseIntegrationTest;
 import eu.bittrade.libs.steemj.IntegrationTest;
+import eu.bittrade.libs.steemj.base.models.AccountName;
+import eu.bittrade.libs.steemj.base.models.BaseTransactionalIntegrationTest;
 import eu.bittrade.libs.steemj.exceptions.SteemCommunicationException;
 
 /**
@@ -14,11 +20,34 @@ import eu.bittrade.libs.steemj.exceptions.SteemCommunicationException;
  * 
  * @author <a href="http://steemit.com/@dez1337">dez1337</a>
  */
-public class ProveAuthorityOperationIT extends BaseIntegrationTest {
+public class ProveAuthorityOperationIT extends BaseTransactionalIntegrationTest {
+    private static final String EXPECTED_TRANSACTION_HEX = "f68585abf4dcebc80457010b0764657a313333371c68747470733a2f2f737465656d69742e636f6d2f4064657a31333"
+            + "33702e5127bd7d41f01d9981a5a2c2524a60706040bbec8838a39719550ea25071000881300000000000003535445454d0000000001000000010000000000000003535445454"
+            + "d000000011c2125f9ad6d2a0a9b5f75ee042f71d44029f361df17be8da017f784b75ebe33152404af3e11c1e2ed853e0e85a58fc9cea4cc9f2d17803766edd5d917a9b21081";
 
+    /**
+     * Prepare the environment for this specific test.
+     * 
+     * @throws Exception
+     *             If something went wrong.
+     */
     @BeforeClass()
     public static void prepareTestClass() throws Exception {
-        setupIntegrationTestEnvironment();
+        setupIntegrationTestEnvironmentForTransactionalTests();
+
+        AccountName challengedAccount = new AccountName("dez1337");
+
+        ProveAuthorityOperation proveAuthorityOperationWithOwnerKey = new ProveAuthorityOperation(challengedAccount,
+                true);
+        ProveAuthorityOperation proveAuthorityOperationWithActiveKey = new ProveAuthorityOperation(challengedAccount);
+
+        ArrayList<Operation> operations = new ArrayList<>();
+        operations.add(proveAuthorityOperationWithOwnerKey);
+        operations.add(proveAuthorityOperationWithActiveKey);
+
+        signedTransaction.setOperations(operations);
+
+        sign();
     }
 
     @Category({ IntegrationTest.class })
@@ -30,12 +59,12 @@ public class ProveAuthorityOperationIT extends BaseIntegrationTest {
     @Category({ IntegrationTest.class })
     @Test
     public void verifyTransaction() throws Exception {
-        // TODO
+        assertThat(steemJ.verifyAuthority(signedTransaction), equalTo(true));
     }
 
     @Category({ IntegrationTest.class })
     @Test
     public void getTransactionHex() throws Exception {
-        // TODO
+        assertThat(steemJ.getTransactionHex(signedTransaction), equalTo(EXPECTED_TRANSACTION_HEX));
     }
 }
