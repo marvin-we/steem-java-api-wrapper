e9c931d1931ffe0f34ab900d64062a90d36ade5e
marvin-we <marvin.weck@web.de>
2017-09-29 22:03:19 +0200

    Work on #94

---
+++ core/src/main/java/eu/bittrade/libs/steemj/base/models/HardforkVersion.java
@@ -0,0 +1,43 @@
+package eu.bittrade.libs.steemj.base.models;
+
+import eu.bittrade.libs.steemj.interfaces.ByteTransformable;
+
+/**
+ * This class is the java implementation of the Steem "hardfork_version" object.
+ * 
+ * @author <a href="http://steemit.com/@dez1337">dez1337</a>
+ */
+public class HardforkVersion extends Version implements ByteTransformable {
+    /**
+     * This class represents a hardfork version.
+     * 
+     * @param majorVersion
+     *            The major version to set.
+     * @param hardforkVersion
+     *            The hardfork version to set.
+     */
+    public HardforkVersion(byte majorVersion, byte hardforkVersion) {
+        super(majorVersion, hardforkVersion, (short) 0);
+    }
+
+    /**
+     * Like {@link #HardforkVersion(byte, byte)}, but expects a Version object.
+     * 
+     * @param hardforkVersion
+     *            The hardfork version to set.
+     */
+    public HardforkVersion(Version hardforkVersion) {
+        super(hardforkVersion.toString());
+    }
+
+    /**
+     * Like {@link #HardforkVersion(byte, byte)}, but expects the hardfork
+     * version in its String representation.
+     * 
+     * @param hardforkVersion
+     *            The hardfork version to set.
+     */
+    public HardforkVersion(String hardforkVersion) {
+        super(hardforkVersion);
+    }
+}
