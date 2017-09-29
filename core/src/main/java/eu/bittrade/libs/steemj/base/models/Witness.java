83e4f1ecb2187910f64c182cbc4ab7838cc07992
marvin-we <marvin.weck@web.de>
2017-09-29 22:03:34 +0200

    Work on #5

--- core/src/main/java/eu/bittrade/libs/steemj/base/models/Witness.java
+++ core/src/main/java/eu/bittrade/libs/steemj/base/models/Witness.java
@@ -1,10 +1,14 @@
 package eu.bittrade.libs.steemj.base.models;
 
 import java.math.BigInteger;
+import java.net.URL;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.bitcoinj.core.Sha256Hash;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import eu.bittrade.libs.steemj.enums.WitnessScheduleType;
 
 /**
  * This class represents a Steem "witness_object" object.
@@ -13,37 +17,85 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class Witness {
     private int id;
-    private String owner;
+    private AccountName owner;
     private TimePointSec created;
-    private String url;
-    private BigInteger votes;
-    @JsonProperty("virtual_last_update")
-    private String virtualLastUpdate;
-    @JsonProperty("virtual_position")
-    private String virtualPosition;
-    @JsonProperty("virtual_scheduled_time")
-    private String virtualScheduledTime;
+    private URL url;
+    // The original type is "uint32_t".
     @JsonProperty("total_missed")
-    private int totalMissed;
+    private long totalMissed;
+    // The original type is "uint64_t".
     @JsonProperty("last_aslot")
-    private int lastAslot;
+    private BigInteger lastAslot;
+    // The original type is "uint64_t".
     @JsonProperty("last_confirmed_block_num")
     private BigInteger lastConfirmedBlockNum;
+    /**
+     * Some witnesses have the job because they did a proof of work, this field
+     * indicates where they were in the POW order. After each round, the witness
+     * with the lowest pow_worker value greater than 0 is removed.
+     */
+    // The original type is "uint64_t".
     @JsonProperty("pow_worker")
-    private int powWorker;
+    private BigInteger powWorker;
     @JsonProperty("signing_key")
-    private String signingKey;
+    private PublicKey signingKey;
     private ChainProperties props;
     @JsonProperty("sbd_exchange_rate")
     private Price sbdExchangeRate;
     @JsonProperty("last_sbd_exchange_update")
     private TimePointSec lastSbdExchangeUpdate;
+    /**
+     * The total votes for this witness. This determines how the witness is
+     * ranked for scheduling. The top N witnesses by votes are scheduled every
+     * round, every one else takes turns being scheduled proportional to their
+     * votes.
+     */
+    // The original type is "share_type" which is an "int64".
+    private long votes;
+    /** How the witness was scheduled the last time it was scheduled. */
+    private WitnessScheduleType schedule;
+    /**
+     * These fields are used for the witness scheduling algorithm which uses
+     * virtual time to ensure that all witnesses are given proportional time for
+     * producing blocks.
+     *
+     * {@link #votes} is used to determine speed. The
+     * {@link #virtualScheduledTime} is the expected time at which this witness
+     * should complete a virtual lap which is defined as the position equal to
+     * 1000 times MAXVOTES.
+     *
+     * virtual_scheduled_time = virtual_last_update + (1000*MAXVOTES -
+     * virtual_position) / votes
+     *
+     * Every time the number of votes changes the virtual_position and
+     * virtual_scheduled_time must update. There is a global current
+     * virtual_scheduled_time which gets updated every time a witness is
+     * scheduled. To update the virtual_position the following math is
+     * performed.
+     *
+     * virtual_position = virtual_position + votes * (virtual_current_time -
+     * virtual_last_update) virtual_last_update = virtual_current_time votes +=
+     * delta_vote virtual_scheduled_time = virtual_last_update + (1000*MAXVOTES
+     * - virtual_position) / votes
+     *
+     */
+    // Original type is "fc::uint128".
+    @JsonProperty("virtual_last_update")
+    private String virtualLastUpdate;
+    // Original type is "fc::uint128".
+    @JsonProperty("virtual_position")
+    private String virtualPosition;
+    // Original type is "fc::uint128".
+    @JsonProperty("virtual_scheduled_time")
+    private BigInteger virtualScheduledTime;
+
+    // Original type is "digest_type" which is a "fc:sha256" object.
     @JsonProperty("last_work")
-    private String lastWork;
+    private Sha256Hash lastWork;
     @JsonProperty("running_version")
-    private String runningVersion;
+    private Version runningVersion;
     @JsonProperty("hardfork_version_vote")
-    private String hardforkVersionVote;
+    private HardforkVersion hardforkVersionVote;
     @JsonProperty("hardfork_time_vote")
     private TimePointSec hardforkTimeVote;
 
@@ -54,82 +106,149 @@ public class Witness {
     private Witness() {
     }
 
+    /**
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
-    public String getVirtualPosition() {
-        return virtualPosition;
-    }
-
-    public String getOwner() {
+    /**
+     * @return the owner
+     */
+    public AccountName getOwner() {
         return owner;
     }
 
+    /**
+     * @return the created
+     */
     public TimePointSec getCreated() {
         return created;
     }
 
-    public String getUrl() {
+    /**
+     * @return the url
+     */
+    public URL getUrl() {
         return url;
     }
 
-    public BigInteger getVotes() {
-        return votes;
-    }
-
-    public String getVirtualLastUpdate() {
-        return virtualLastUpdate;
-    }
-
-    public String getVirtualScheduledTime() {
-        return virtualScheduledTime;
-    }
-
-    public int getTotalMissed() {
+    /**
+     * @return the totalMissed
+     */
+    public long getTotalMissed() {
         return totalMissed;
     }
 
-    public int getLastAslot() {
+    /**
+     * @return the lastAslot
+     */
+    public BigInteger getLastAslot() {
         return lastAslot;
     }
 
+    /**
+     * @return the lastConfirmedBlockNum
+     */
     public BigInteger getLastConfirmedBlockNum() {
         return lastConfirmedBlockNum;
     }
 
-    public int getPowWorker() {
+    /**
+     * @return the powWorker
+     */
+    public BigInteger getPowWorker() {
         return powWorker;
     }
 
-    public String getSigningKey() {
+    /**
+     * @return the signingKey
+     */
+    public PublicKey getSigningKey() {
         return signingKey;
     }
 
+    /**
+     * @return the props
+     */
     public ChainProperties getProps() {
         return props;
     }
 
+    /**
+     * @return the sbdExchangeRate
+     */
     public Price getSbdExchangeRate() {
         return sbdExchangeRate;
     }
 
+    /**
+     * @return the lastSbdExchangeUpdate
+     */
     public TimePointSec getLastSbdExchangeUpdate() {
         return lastSbdExchangeUpdate;
     }
 
-    public String getLastWork() {
+    /**
+     * @return the votes
+     */
+    public long getVotes() {
+        return votes;
+    }
+
+    /**
+     * @return the schedule
+     */
+    public WitnessScheduleType getSchedule() {
+        return schedule;
+    }
+
+    /**
+     * @return the virtualLastUpdate
+     */
+    public String getVirtualLastUpdate() {
+        return virtualLastUpdate;
+    }
+
+    /**
+     * @return the virtualPosition
+     */
+    public String getVirtualPosition() {
+        return virtualPosition;
+    }
+
+    /**
+     * @return the virtualScheduledTime
+     */
+    public BigInteger getVirtualScheduledTime() {
+        return virtualScheduledTime;
+    }
+
+    /**
+     * @return the lastWork
+     */
+    public Sha256Hash getLastWork() {
         return lastWork;
     }
 
-    public String getRunningVersion() {
+    /**
+     * @return the runningVersion
+     */
+    public Version getRunningVersion() {
         return runningVersion;
     }
 
-    public String getHardforkVersionVote() {
+    /**
+     * @return the hardforkVersionVote
+     */
+    public HardforkVersion getHardforkVersionVote() {
         return hardforkVersionVote;
     }
 
+    /**
+     * @return the hardforkTimeVote
+     */
     public TimePointSec getHardforkTimeVote() {
         return hardforkTimeVote;
     }
