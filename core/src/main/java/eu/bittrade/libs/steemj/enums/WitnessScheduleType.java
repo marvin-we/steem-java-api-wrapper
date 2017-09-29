2327970565297fe5ff983f86aacdf6be4e1e1f7d
marvin-we <marvin.weck@web.de>
2017-09-29 21:52:34 +0200

    Fix #98

---
+++ core/src/main/java/eu/bittrade/libs/steemj/enums/WitnessScheduleType.java
@@ -0,0 +1,10 @@
+package eu.bittrade.libs.steemj.enums;
+
+/**
+ * An enumeration for all existing witness schedule types.
+ * 
+ * @author <a href="http://steemit.com/@dez1337">dez1337</a>
+ */
+public enum WitnessScheduleType {
+    TOP19, TIMESHARE, MINER, NONE
+}
