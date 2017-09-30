b85134acaba425827f02029f4f89113626354f57
marvin-we <marvin.weck@web.de>
2017-09-30 11:51:59 +0200

    Work on #88

--- core/src/main/java/eu/bittrade/libs/steemj/base/models/operations/AbstractTransferOperation.java
+++ core/src/main/java/eu/bittrade/libs/steemj/base/models/operations/AbstractTransferOperation.java
@@ -1,11 +1,15 @@
 package eu.bittrade.libs.steemj.base.models.operations;
 
+import java.security.InvalidParameterException;
+import java.util.List;
+import java.util.Map;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import eu.bittrade.libs.steemj.annotations.SignatureRequired;
 import eu.bittrade.libs.steemj.base.models.AccountName;
 import eu.bittrade.libs.steemj.base.models.Asset;
 import eu.bittrade.libs.steemj.enums.PrivateKeyType;
+import eu.bittrade.libs.steemj.interfaces.SignatureObject;
 
 /**
  * This abstract class contains fields that exist in all Steem
@@ -14,7 +18,6 @@ import eu.bittrade.libs.steemj.enums.PrivateKeyType;
  * @author <a href="http://steemit.com/@dez1337">dez1337</a>
  */
 abstract class AbstractTransferOperation extends Operation {
-    @SignatureRequired(type = PrivateKeyType.ACTIVE)
     @JsonProperty("from")
     protected AccountName from;
     @JsonProperty("to")
@@ -27,5 +30,92 @@ abstract class AbstractTransferOperation extends Operation {
      */
     protected AbstractTransferOperation(boolean virtual) {
         super(virtual);
+    }
+
+    /**
+     * Get the account name of the user who transfered the <code>amount</code>.
+     * 
+     * @return The user which transfered the <code>amount</code>.
+     */
+    public AccountName getFrom() {
+        return from;
+    }
+
+    /**
+     * Set the account name of the user who transfers the <code>amount</code>.
+     * <b>Notice:</b> The private active key of this account needs to be stored
+     * in the key storage.
+     * 
+     * @param from
+     *            The account name of the user who will transfer the
+     *            <code>amount</code>.
+     * @throws InvalidParameterException
+     *             If the <code>from</code> account is null.
+     */
+    public void setFrom(AccountName from) {
+        if (from == null) {
+            throw new InvalidParameterException("The from account can't be null.");
+        }
+
+        this.from = from;
+    }
+
+    /**
+     * Get the account name of the user who received the <code>amount</code>.
+     * 
+     * @return The account name of the user who received the
+     *         <code>amount</code>.
+     */
+    public AccountName getTo() {
+        return to;
+    }
+
+    /**
+     * Set the account name of the user who will received the
+     * <code>amount</code>.
+     * 
+     * @param to
+     *            The account name of the user who will received the
+     *            <code>amount</code>.
+     * @throws InvalidParameterException
+     *             If the <code>to</code> account is null.
+     */
+    public void setTo(AccountName to) {
+        if (to == null) {
+            throw new InvalidParameterException("The to account can't be null.");
+        }
+
+        this.to = to;
+    }
+
+    /**
+     * Get the <code>amount</code> of that has been send.
+     * 
+     * @return The <code>amount</code> of that has been send.
+     */
+    public Asset getAmount() {
+        return amount;
+    }
+
+    /**
+     * Set the <code>amount</code> of that will be send.
+     * 
+     * @param amount
+     *            The <code>amount</code> of that will be send.
+     * @throws InvalidParameterException
+     *             If the <code>amount</code> is null.
+     */
+    public void setAmount(Asset amount) {
+        if (amount == null) {
+            throw new InvalidParameterException("The amount can't be null.");
+        }
+
+        this.amount = amount;
+    }
+
+    @Override
+    public Map<SignatureObject, List<PrivateKeyType>> getRequiredAuthorities(
+            Map<SignatureObject, List<PrivateKeyType>> requiredAuthoritiesBase) {
+        return mergeRequiredAuthorities(requiredAuthoritiesBase, this.getFrom(), PrivateKeyType.ACTIVE);
     }
 }
