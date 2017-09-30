d59c0abdaee1f0bb0ac1432d02895236253badd5
marvin-we <marvin.weck@web.de>
2017-09-30 13:19:58 +0200

    Work on #88

---
+++ core/src/main/java/eu/bittrade/libs/steemj/base/models/operations/AbstractAccountOperation.java
@@ -0,0 +1,110 @@
+package eu.bittrade.libs.steemj.base.models.operations;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import eu.bittrade.libs.steemj.base.models.Authority;
+import eu.bittrade.libs.steemj.base.models.PublicKey;
+
+/**
+ * This abstract class contains fields that exist in all Steem Operations
+ * related to the account creation / update.
+ * 
+ * @author <a href="http://steemit.com/@dez1337">dez1337</a>
+ */
+public abstract class AbstractAccountOperation extends Operation {
+    @JsonProperty("owner")
+    private Authority owner;
+    @JsonProperty("active")
+    private Authority active;
+    @JsonProperty("posting")
+    private Authority posting;
+    @JsonProperty("memo_key")
+    private PublicKey memoKey;
+    @JsonProperty("json_metadata")
+    private String jsonMetadata;
+
+    /**
+     * Create a new Operation object by providing the operation type.
+     */
+    protected AbstractAccountOperation(boolean virtual) {
+        super(virtual);
+    }
+
+    /**
+     * Get the owner {@link eu.bittrade.libs.steemj.base.models.Authority
+     * Authority}.
+     * 
+     * @return The owner authority.
+     */
+    public abstract Authority getOwner();
+
+    /**
+     * Set the owner {@link eu.bittrade.libs.steemj.base.models.Authority
+     * Authority}.
+     * 
+     * @param owner
+     *            The owner authority.
+     */
+    public abstract void setOwner(Authority owner);
+
+    /**
+     * Get the active {@link eu.bittrade.libs.steemj.base.models.Authority
+     * Authority}.
+     * 
+     * @return The active authority.
+     */
+    public abstract Authority getActive();
+
+    /**
+     * Set the active {@link eu.bittrade.libs.steemj.base.models.Authority
+     * Authority}.
+     * 
+     * @param active
+     *            The active authority.
+     */
+    public abstract void setActive(Authority active);
+
+    /**
+     * Get the posting {@link eu.bittrade.libs.steemj.base.models.Authority
+     * Authority}.
+     * 
+     * @return The posting authority.
+     */
+    public abstract Authority getPosting();
+
+    /**
+     * Set the posting {@link eu.bittrade.libs.steemj.base.models.Authority
+     * Authority}.
+     * 
+     * @param posting
+     *            The posting authority.
+     */
+    public abstract void setPosting(Authority posting);
+
+    /**
+     * Get the memo {@link eu.bittrade.libs.steemj.base.models.PublicKey
+     * PublicKey}.
+     * 
+     * @return The memo key.
+     */
+    public abstract PublicKey getMemoKey();
+
+    /**
+     * Get the json metadata that has been added to this operation.
+     * 
+     * @return The json metadata that has been added to this operation.
+     */
+    public String getJsonMetadata() {
+        return jsonMetadata;
+    }
+
+    /**
+     * Add json metadata to this operation.
+     * 
+     * @param jsonMetadata
+     *            The json metadata.
+     */
+    public void setJsonMetadata(String jsonMetadata) {
+        this.jsonMetadata = jsonMetadata;
+    }
+}
