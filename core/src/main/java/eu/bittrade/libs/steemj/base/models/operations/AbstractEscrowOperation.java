27a99cfb2904472da3e875c01f3f31f1bf3f6e57
marvin-we <marvin.weck@web.de>
2017-09-30 15:06:54 +0200

    Fix #88

--- core/src/main/java/eu/bittrade/libs/steemj/base/models/operations/AbstractEscrowOperation.java
+++ core/src/main/java/eu/bittrade/libs/steemj/base/models/operations/AbstractEscrowOperation.java
@@ -1,10 +1,13 @@
 package eu.bittrade.libs.steemj.base.models.operations;
 
+import java.util.List;
+import java.util.Map;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import eu.bittrade.libs.steemj.annotations.SignatureRequired;
 import eu.bittrade.libs.steemj.base.models.AccountName;
 import eu.bittrade.libs.steemj.enums.PrivateKeyType;
+import eu.bittrade.libs.steemj.interfaces.SignatureObject;
 
 /**
  * This abstract class contains fields that exist in all Steem
@@ -13,8 +16,11 @@ import eu.bittrade.libs.steemj.enums.PrivateKeyType;
  * @author <a href="http://steemit.com/@dez1337">dez1337</a>
  */
 abstract class AbstractEscrowOperation extends Operation {
+    @JsonProperty("from")
     protected AccountName from;
+    @JsonProperty("to")
     protected AccountName to;
+    @JsonProperty("agent")
     protected AccountName agent;
     // Original type is unit32_t so we use long here.
     @JsonProperty("escrow_id")
@@ -27,4 +33,88 @@ abstract class AbstractEscrowOperation extends Operation {
         super(virtual);
     }
 
+    /**
+     * Get the account who wants to transfer the fund to the {@link #getTo() to}
+     * account.
+     * 
+     * @return The account who wants to transfer the fund.
+     */
+    public AccountName getFrom() {
+        return from;
+    }
+
+    /**
+     * Set the account who wants to transfer the fund to the {@link #getTo() to}
+     * account. <b>Notice:</b> The private active key of this account needs to
+     * be stored in the key storage.
+     * 
+     * @param from
+     *            The account who wants to transfer the fund.
+     */
+    public void setFrom(AccountName from) {
+        this.from = from;
+    }
+
+    /**
+     * Get the account who should receive the funds.
+     * 
+     * @return The account who should receive the funds.
+     */
+    public AccountName getTo() {
+        return to;
+    }
+
+    /**
+     * Set the account who should receive the funds.
+     * 
+     * @param to
+     *            The account who should receive the funds.
+     */
+    public void setTo(AccountName to) {
+        this.to = to;
+    }
+
+    /**
+     * Get the agent account.
+     * 
+     * @return The agent account.
+     */
+    public AccountName getAgent() {
+        return agent;
+    }
+
+    /**
+     * Set the agent account.
+     * 
+     * @param agent
+     *            The agent account.
+     */
+    public void setAgent(AccountName agent) {
+        this.agent = agent;
+    }
+
+    /**
+     * Get the unique id of this escrow operation.
+     * 
+     * @return The unique id of this escrow operation.
+     */
+    public int getEscrowId() {
+        return (int) escrowId;
+    }
+
+    /**
+     * Set the unique id of this escrow operation.
+     * 
+     * @param escrowId
+     *            The unique id of this escrow operation.
+     */
+    public void setEscrowId(long escrowId) {
+        this.escrowId = escrowId;
+    }
+
+    @Override
+    public Map<SignatureObject, List<PrivateKeyType>> getRequiredAuthorities(
+            Map<SignatureObject, List<PrivateKeyType>> requiredAuthoritiesBase) {
+        return mergeRequiredAuthorities(requiredAuthoritiesBase, this.getFrom(), PrivateKeyType.ACTIVE);
+    }
 }
---
+++ core/src/main/java/eu/bittrade/libs/steemj/base/models/operations/AbstractLimitOrderOperation.java
@@ -0,0 +1,69 @@
+package eu.bittrade.libs.steemj.base.models.operations;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import eu.bittrade.libs.steemj.base.models.AccountName;
+import eu.bittrade.libs.steemj.enums.PrivateKeyType;
+import eu.bittrade.libs.steemj.interfaces.SignatureObject;
+
+/**
+ * This abstract class contains fields that exist in all Steem
+ * "limit_order_operation" objects.
+ * 
+ * @author <a href="http://steemit.com/@dez1337">dez1337</a>
+ */
+public abstract class AbstractLimitOrderOperation extends Operation {
+    @JsonProperty("owner")
+    private AccountName owner;
+    // Type is uint32 in the original code, but has to be long here as Java does
+    // not support unsigned numbers accurate.
+    @JsonProperty("orderid")
+    private long orderId;
+
+    /**
+     * Create a new Operation object by providing the operation type.
+     */
+    protected AbstractLimitOrderOperation(boolean virtual) {
+        super(virtual);
+    }
+
+    /**
+     * Get the owner of this order.
+     * 
+     * @return The owner of the order.
+     */
+    public abstract AccountName getOwner();
+
+    /**
+     * Set the owner for this order.
+     * 
+     * @param owner
+     *            The owner for this order.
+     */
+    public abstract void setOwner(AccountName owner);
+
+    /**
+     * Get the id of this order.
+     * 
+     * @return The id of this order.
+     */
+    public abstract int getOrderId();
+
+    /**
+     * Set the id of this order. The only limitation for this id is that it has
+     * to be free, meaning that there is no other open order with this id.
+     * 
+     * @param orderId
+     *            The id of this order.
+     */
+    public abstract void setOrderId(long orderId);
+
+    @Override
+    public Map<SignatureObject, List<PrivateKeyType>> getRequiredAuthorities(
+            Map<SignatureObject, List<PrivateKeyType>> requiredAuthoritiesBase) {
+        return mergeRequiredAuthorities(requiredAuthoritiesBase, this.getOwner(), PrivateKeyType.ACTIVE);
+    }
+}
