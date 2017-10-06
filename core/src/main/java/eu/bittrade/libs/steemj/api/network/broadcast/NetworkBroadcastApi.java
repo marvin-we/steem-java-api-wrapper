d3be47d2417b05f5706869001aea28eaced1fff2
marvin-we <marvin.weck@web.de>
2017-10-06 11:52:41 +0200

    Work on #76

---
+++ core/src/main/java/eu/bittrade/libs/steemj/api/network/broadcast/NetworkBroadcastApi.java
@@ -0,0 +1,64 @@
+package eu.bittrade.libs.steemj.api.network.broadcast;
+
+import eu.bittrade.libs.steemj.base.models.SignedTransaction;
+import eu.bittrade.libs.steemj.communication.dto.RequestWrapperDTO;
+import eu.bittrade.libs.steemj.enums.RequestMethods;
+import eu.bittrade.libs.steemj.enums.SteemApis;
+import eu.bittrade.libs.steemj.exceptions.SteemCommunicationException;
+
+/**
+ * This class implements the network broadcast api which is required to send
+ * transactions.
+ * 
+ * @author <a href="http://steemit.com/@dez1337">dez1337</a>
+ */
+public class NetworkBroadcastApi {
+    /** Add a private constructor to hide the implicit public one. */
+    private NetworkBroadcastApi() {
+    }
+
+    /*
+     * TODO: Implement all methods of this API: (broadcast_transaction)
+     * (broadcast_transaction_with_callback) (broadcast_transaction_synchronous)
+     * (broadcast_block) (set_max_block_age)
+     */
+    /**
+     * Broadcast a transaction on the Steem blockchain.
+     * 
+     * @param transaction
+     *            A transaction object that has been signed.
+     * @throws SteemCommunicationException
+     *             <ul>
+     *             <li>If the server was not able to answer the request in the
+     *             given time (see
+     *             {@link eu.bittrade.libs.steemj.configuration.SteemJConfig#setTimeout(long)
+     *             setTimeout})</li>
+     *             <li>If there is a connection problem.</li>
+     *             <li>If the SteemJ is unable to transform the JSON response
+     *             into a Java object.</li>
+     *             <li>If the Server returned an error object.</li>
+     *             </ul>
+     */
+    public void broadcastTransaction(SignedTransaction transaction) throws SteemCommunicationException {
+        RequestWrapperDTO requestObject = new RequestWrapperDTO();
+        requestObject.setApiMethod(RequestMethods.BROADCAST_TRANSACTION);
+        requestObject.setSteemApi(SteemApis.NETWORK_BROADCAST_API);
+
+        // TODO: transaction.sign();
+        Object[] parameters = { transaction };
+        requestObject.setAdditionalParameters(parameters);
+
+        communicationHandler.performRequest(requestObject, Object.class);
+    }
+
+    // TODO implement this!
+    public Boolean broadcastTransactionSynchronous(SignedTransaction transaction) throws SteemCommunicationException {
+        RequestWrapperDTO requestObject = new RequestWrapperDTO();
+        requestObject.setApiMethod(RequestMethods.BROADCAST_TRANSACTION_SYNCHRONOUS);
+        requestObject.setSteemApi(SteemApis.NETWORK_BROADCAST_API);
+        Object[] parameters = { transaction };
+        requestObject.setAdditionalParameters(parameters);
+
+        return null;
+    }
+}
