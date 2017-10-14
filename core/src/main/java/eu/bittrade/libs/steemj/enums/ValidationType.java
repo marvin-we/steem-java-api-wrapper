ebae67fd61cd8c7b8135377c477af548ee508bb2
marvin-we <marvin.weck@web.de>
2017-10-14 18:42:37 +0200

    Work on #102

---
+++ core/src/main/java/eu/bittrade/libs/steemj/enums/ValidationType.java
@@ -0,0 +1,13 @@
+package eu.bittrade.libs.steemj.enums;
+
+/**
+ * An enumeration for all existing validation types.
+ * 
+ * @author <a href="http://steemit.com/@dez1337">dez1337</a>
+ */
+public enum ValidationType {
+    /** Indicates that the validation should be skipped. */
+    SKIP_VALIDATION,
+    /** Indicates that all fields should be validated. */
+    COMPLETE
+}
---
+++ core/src/main/java/eu/bittrade/libs/steemj/interfaces/Validatable.java
@@ -0,0 +1,23 @@
+package eu.bittrade.libs.steemj.interfaces;
+
+import java.security.InvalidParameterException;
+
+import eu.bittrade.libs.steemj.enums.ValidationType;
+
+/**
+ * This interface is used to make sure an object implements the validate method.
+ * 
+ * @author <a href="http://steemit.com/@dez1337">dez1337</a>
+ */
+@FunctionalInterface
+public interface Validatable {
+    /**
+     * Use this method to verify that this object is valid.
+     * 
+     * @param validationType
+     *            An indicator telling the method what should be validated.
+     * @throws InvalidParameterException
+     *             If a field does not fulfill the requirements.
+     */
+    public void validate(ValidationType validationType);
+}
