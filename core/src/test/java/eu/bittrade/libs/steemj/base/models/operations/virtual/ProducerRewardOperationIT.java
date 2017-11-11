b0d755f0d153f34e7cdb48f221ecc802decbe16b
marvin-we <marvin.weck@web.de>
2017-11-11 14:17:54 +0100

    Fix format

--- core/src/test/java/eu/bittrade/libs/steemj/base/models/operations/virtual/ProducerRewardOperationIT.java
+++ core/src/test/java/eu/bittrade/libs/steemj/base/models/operations/virtual/ProducerRewardOperationIT.java
@@ -58,5 +58,4 @@ public class ProducerRewardOperationIT extends BaseOperationParsingIT {
         assertThat(((ProducerRewardOperation) producerRewardOperation).getVestingShares().getAmount(),
                 equalTo(EXPECTED_VESTS_VALUE));
     }
-
 }
