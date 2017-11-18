dd9cd717d2bfaed1a4d80385ae96cda1a1cfa096
marvin-we <marvin.weck@web.de>
2017-11-18 20:35:20 +0100

    Work on #140 and #152

--- core/src/main/java/eu/bittrade/libs/steemj/plugins/witness/model/operations/EnableContentEditingOperation.java
+++ core/src/main/java/eu/bittrade/libs/steemj/plugins/witness/model/operations/EnableContentEditingOperation.java
@@ -1,5 +1,7 @@
 package eu.bittrade.libs.steemj.plugins.witness.model.operations;
 
+import java.security.InvalidParameterException;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -7,6 +9,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import eu.bittrade.libs.steemj.base.models.AccountName;
 import eu.bittrade.libs.steemj.base.models.TimePointSec;
+import eu.bittrade.libs.steemj.base.models.operations.CustomJsonOperation;
 import eu.bittrade.libs.steemj.base.models.operations.CustomJsonOperationPayload;
 import eu.bittrade.libs.steemj.enums.ValidationType;
 
@@ -21,6 +24,19 @@ public class EnableContentEditingOperation extends CustomJsonOperationPayload {
     @JsonProperty("relock_time")
     private TimePointSec relockTime;
 
+    /**
+     * Use this operation to enable the editing of posts who are already
+     * disabled.
+     * 
+     * The {@link CustomJsonOperation} that will carry this operation needs to
+     * be signed with the active key of the <code>account</code> to prevent
+     * stolen posting keys from being used to deface old content.
+     * 
+     * @param account
+     *            The account to enable the editing for.
+     * @param relockTime
+     *            The time when editing is locked again.
+     */
     @JsonCreator
     public EnableContentEditingOperation(@JsonProperty("account") AccountName account,
             @JsonProperty("relock_time") TimePointSec relockTime) {
@@ -29,33 +45,45 @@ public class EnableContentEditingOperation extends CustomJsonOperationPayload {
     }
 
     /**
-     * @return the account
+     * Get the account whose posts are editable again.
+     * 
+     * @return The account whose posts are editable again.
      */
     public AccountName getAccount() {
         return account;
     }
 
     /**
+     * Set the <code>account</code> whose posts should be editable again.
+     * 
      * @param account
-     *            the account to set
+     *            The account whose posts should be editable again.
+     * @throws InvalidParameterException
+     *             If the <code>account</code> is <code>null</code>.
      */
     public void setAccount(AccountName account) {
-        this.account = account;
+        this.account = setIfNotNull(account, "The account cannot be null");
     }
 
     /**
-     * @return the relockTime
+     * Get the point in time when posts are locked again.
+     * 
+     * @return The point in time when posts are locked again.
      */
     public TimePointSec getRelockTime() {
         return relockTime;
     }
 
     /**
+     * Set the point in time when posts are locked again.
+     * 
      * @param relockTime
-     *            the relockTime to set
+     *            The point in time when posts are locked again.
+     * @throws InvalidParameterException
+     *             If the <code>relockTime</code> is <code>null</code>.
      */
     public void setRelockTime(TimePointSec relockTime) {
-        this.relockTime = relockTime;
+        this.relockTime = setIfNotNull(relockTime, "The relock time cannot be null");
     }
 
     @Override
@@ -65,7 +93,6 @@ public class EnableContentEditingOperation extends CustomJsonOperationPayload {
 
     @Override
     public void validate(ValidationType validationType) {
-        // TODO Auto-generated method stub
-
+        // No additional validation required.
     }
 }
