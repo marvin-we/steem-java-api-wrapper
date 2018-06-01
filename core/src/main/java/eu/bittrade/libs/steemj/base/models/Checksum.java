bc1d2a1374060e1e8e5b1ac2e6abd07150bb2805
Leather Dog Muksihs <muksihs@gmail.com>
2018-06-01 18:39:58 -0400

    unshadow

--- core/src/main/java/eu/bittrade/libs/steemj/base/models/Checksum.java
+++ core/src/main/java/eu/bittrade/libs/steemj/base/models/Checksum.java
@@ -11,16 +11,6 @@ import eu.bittrade.libs.steemj.interfaces.HasJsonAnyGetterSetter;
  * @author <a href="http://steemit.com/@dez1337">dez1337</a>
  */
 public class Checksum extends Ripemd160 {
-	private final Map<String, Object> _anyGetterSetterMap = new HashMap<>();
-	@Override
-	public Map<String, Object> _getter() {
-		return _anyGetterSetterMap;
-	}
-
-	@Override
-	public void _setter(String key, Object value) {
-		_getter().put(key, value);
-	}
 
     /** Generated serial version uid. */
     private static final long serialVersionUID = 4420141835144717006L;
