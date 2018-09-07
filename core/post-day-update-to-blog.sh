f1383d96973a9a592886cf7180d9d50c617cdc68
Leather Dog Muksihs <muksihs@gmail.com>
2018-09-07 17:00:58 -0400

    Adjust post day update to blog script to be more configurable.
    Auto set git repo name using git remote -v as part of post script.

--- core/post-day-update-to-blog.sh
+++ core/post-day-update-to-blog.sh
@@ -5,6 +5,20 @@ set -o pipefail
 
 cd "$(dirname "$0")"
 
+TEST_MODE=0 #Set to 0 to do a live post.
+
+#preferred repo name
+REPO="$(git remote -v|grep origin|grep fetch|sed 's/\t/ /g'|cut -f 2 -d ' ')"
+REPO="$(basename $REPO|sed 's/.git$//')"
+
+#override auto detected repo name
+#REPO="steem-java-api-wrapper"
+
+TAGS="utopian-io steemdev java steemj steem"
+
+AUTH_FILE="${HOME}/.steem/steem.properties"
+STEEM_CLI_POSTER=${HOME}/git/SteemCliPoster/build/libs/SteemCliPoster.jar
+
 when=$(git log -1 --pretty=format:'%ci')
 
 mkdir tmp 2> /dev/null || true
@@ -16,14 +30,14 @@ if [ ! -s tmp/log.new ]; then exit; fi
 echo "Posting new message"
 msgfile="tmp/msg.txt"
 cp /dev/null "$msgfile"
-echo "title: Updates for $(basename $(pwd)) within the past day. $when"  >> "$msgfile"
-echo "tags: utopian-io steemdev java steemj steem"  >> "$msgfile"
+echo "title: Updates for ${REPO} within the past day. $when"  >> "$msgfile"
+echo "tags: ${TAGS}"  >> "$msgfile"
 echo "format: markdown"  >> "$msgfile"
 echo "" >> "$msgfile"
 echo "![SteemJ Logo](https://cdn.steemitimages.com/DQmbwwNH2fZVqPEyNmDFRbimZaPPU3RjcdtEh4uLBDmg1DL/image.png)" >> "$msgfile"
 echo "" >> "$msgfile"
-echo "## $(basename $(pwd))" >> "$msgfile"
-echo "Updates for $(basename $(pwd)) in the past day. $when" >> "$msgfile"
+echo "## ${REPO}" >> "$msgfile"
+echo "Updates for ${REPO} in the past day. $when" >> "$msgfile"
 echo "" >> "$msgfile"
 echo "" >> "$msgfile"
 git log --branches=\* --after="1 day ago" | sed 's/<.*@.*>/[email redacted]/g' | sed 's/^commit /#### commit /g' >> "$msgfile"
@@ -34,9 +48,13 @@ sed -i 's/</\&lt;/g' "$msgfile"
 sed -i 's/>/\&gt;/g' "$msgfile"
 sed -i 's/\t/    /g' "$msgfile"
 sed -i 's|  |\&nbsp; |g' "$msgfile"
-java -jar ~/git/SteemCliPoster/build/libs/SteemCliPoster.jar \
-	--auth-file ~/.steem/steem.properties \
-	--no-escape \
-	--file "$msgfile"
-cp tmp/log.new tmp/log.old
 
+if [ ${TEST_MODE} = 0 ]; then
+	java -jar ${STEEM_CLI_POSTER} \
+		--auth-file ${AUTH_FILE} \
+		--no-escape \
+		--file "$msgfile"
+	cp tmp/log.new tmp/log.old
+fi
+
+exit 0
