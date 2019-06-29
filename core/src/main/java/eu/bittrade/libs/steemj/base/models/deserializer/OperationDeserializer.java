b39ae926abffe4a527264d23c540112bd0ccd354
Radosław Pańczyk <radoslawpanczyk@gmail.com>
2019-06-29 14:37:09 +0200

    Revert "Operationdeserializer fix"
    This reverts commit a4a5dbe7860dfd481b99274e0a349252d41f5069.

--- core/src/main/java/eu/bittrade/libs/steemj/base/models/deserializer/OperationDeserializer.java
+++ core/src/main/java/eu/bittrade/libs/steemj/base/models/deserializer/OperationDeserializer.java
@@ -1,11 +1,9 @@
 package eu.bittrade.libs.steemj.base.models.deserializer;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.bittrade.libs.steemj.base.models.operations.Operation;
 
 import java.io.IOException;
@@ -13,9 +11,9 @@ import java.io.IOException;
 public class OperationDeserializer extends JsonDeserializer<Operation> {
 
     @Override
-    public Operation deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException {
+    public Operation deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode node = jp.getCodec().readTree(jp);
+        JsonNode node = arg0.readValueAsTree();
 
         try {
             return mapper.readValue(node.toString(), Operation.class);
